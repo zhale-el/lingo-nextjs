9ab0b8de87328784625e5f52e2867490b0d90b54
zhaleh <zhalehelmi@gmail.com>
2024-09-07 17:20:04 +0330

    header footer

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+  return (
+    <div className="h-20 w-full border-b-2 border-slate-200 px-4">header</div>
+  );
+};
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <h1>home</h1>;
+  return <h1>This is marketing page</h1>;
 };
 
 export default Home;
