14fe4e969ca9d67f9d842755f3adc763bf73b115
zhaleh <zhalehelmi@gmail.com>
2024-09-08 19:53:19 +0330

    add auth

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+
 export const Header = () => {
   return (
-    <div className="h-20 w-full border-b-2 border-slate-200 px-4">header</div>
+    <div className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" width={40} height={40} alt="Mascot" />
+
+          <h1></h1>
+        </div>
+      </div>
+    </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -26,8 +27,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
