200df16885200619e097b93fd143827a470523d9
zhaleh <zhalehelmi@gmail.com>
2024-09-24 20:57:54 +0330

    lessonbutton

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import Link from "next/link";
+
+import { Check, Crown, Star } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+import { CircularProgressbarWithChildren } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
 type Props = {
   id: number;
   index: number;
@@ -17,5 +27,38 @@ export const LessonButton = ({
   current,
   percentage,
 }: Props) => {
-  return <div>Enter</div>;
+  const cycleLength = 8;
+  const cycleIndex = index % cycleLength;
+
+  let indentationLevel;
+
+  if (cycleIndex <= 2) {
+    indentationLevel = cycleIndex;
+  } else if (cycleIndex <= 4) {
+    indentationLevel = 4 - cycleIndex;
+  } else if (cycleIndex <= 6) {
+    indentationLevel = 4 - cycleIndex;
+  } else {
+    indentationLevel = cycleIndex - 8;
+  }
+
+  const rightPosition = indentationLevel * 40;
+
+  const isFirst = index === 0;
+  const isLast = index === totalCount;
+  const isCompleted = !current && !locked;
+
+  const Icon = isCompleted ? Check : isLast ? Crown : Star;
+
+  const href = isCompleted ? `/lesson/${id}` : "/lesson";
+
+  return (
+    <Link
+      href={href}
+      aria-disabled={locked}
+      style={{ pointerEvents: locked ? "none" : "auto" }}
+    >
+      {id}
+    </Link>
+  );
 };
