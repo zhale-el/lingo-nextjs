93a2356035bd9403efd3de75204bb47e9c943123
zhaleh <zhalehelmi@gmail.com>
2024-10-09 15:44:45 +0330

    stripe

--- db/queries.ts
+++ db/queries.ts
@@ -10,6 +10,7 @@ import {
   lessons,
   units,
   userProgress,
+  userSubscription,
 } from "./schema";
 
 export const getUserProgress = cache(async () => {
@@ -206,4 +207,26 @@ export const getLessonPercentage = cache(async () => {
   );
 
   return percentage;
+});
+
+const DAY_IN_MS = 86_400_000;
+
+export const getUserSubscription = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userSubscription.findFirst({
+    where: eq(userSubscription.userId, userId),
+  });
+  if (!data) return null;
+
+  const isActive =
+    data.stripePriceId &&
+    data.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+
+  return {
+    ...data,
+    isActive: !!isActive,
+  };
 });
